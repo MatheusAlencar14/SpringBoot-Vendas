3f1cf0840faa0c183b8af774d7456374971757a5
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-10-05 09:23:45 -0300

    Injeção de Dependências com Spring Boot

---
+++ src/main/java/com/dev/repository/ClientesRepository.java
@@ -0,0 +1,10 @@
+package com.dev.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir() {
+        //acessa a base e salva o cliente
+    }
+}
