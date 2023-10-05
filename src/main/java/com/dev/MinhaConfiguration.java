005c28620498d92f56dd2362f61bc54bf955e71e
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-10-05 09:49:13 -0300

    Implementando multiplos ambientes e profiles

--- src/main/java/com/dev/MinhaConfiguration.java
+++ src/main/java/com/dev/MinhaConfiguration.java
@@ -1,13 +1,18 @@
 package com.dev;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development") //Isso indica que essa clase só vai funcionar no ambiente development.properties
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("INICIALIZANDO CONFIG DE DESENVOLVIMENTO");
+        };
     }
 }
