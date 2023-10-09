1c4ca644c1b2270541c8add6a6326f5681eaf34d
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-10-09 11:55:30 -0300

    Mapeando a Entidade Cliente para JPA

--- src/main/java/com/dev/domain/entity/Cliente.java
+++ src/main/java/com/dev/domain/entity/Cliente.java
@@ -1,8 +1,17 @@
 package com.dev.domain.entity;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.TypeAlias;
+
+@Entity //Mapeando que essa é uma entidade no JPA
+@Table(name = "cliente") //Indicando qual o nome da tabela no Banco de Dados
 public class Cliente {
 
+    @Id //Mapeando que esse atributo é um ID
+    @GeneratedValue(strategy = GenerationType.AUTO) //Indicando a estratégia de auto incremento
+    @Column(name = "id") //Indicando o nome da coluna (como é o mesmo nome, não seria necessário)
     private Integer id;
+    @Column(name = "nome", length = 100) //Indicando o nome e o tamanho
     private String nome;
 
     public Cliente() {
