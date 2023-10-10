8abaea5da25e351aba9f492043c950d5be3d8c7b
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-10-10 11:33:12 -0300

    Mapeando a Entidade Produto com annotations JPA

--- src/main/java/com/dev/domain/entity/Produto.java
+++ src/main/java/com/dev/domain/entity/Produto.java
@@ -1,11 +1,22 @@
 package com.dev.domain.entity;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao", length = 100)
     private String descricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 
     public Produto() {
