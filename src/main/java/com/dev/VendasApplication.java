a8df58d5e42c5e5682c77db076cb76291d1ba724
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-10-17 09:59:31 -0300

    Implementando Requisição GET com parâmetros e ResponseEntity

--- src/main/java/com/dev/VendasApplication.java
+++ src/main/java/com/dev/VendasApplication.java
@@ -1,12 +1,28 @@
 package com.dev;
 
+import com.dev.domain.entity.Cliente;
+import com.dev.domain.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired ClientesRepository clientesRepository) {
+        return args -> {
+            Cliente c1 = new Cliente(null, "Matheus");
+            Cliente c2 = new Cliente(null, "Miguel");
+            clientesRepository.saveAll(Arrays.asList(c1, c2));
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
--- src/main/java/com/dev/rest/controller/ClienteController.java
+++ src/main/java/com/dev/rest/controller/ClienteController.java
@@ -1,18 +1,30 @@
 package com.dev.rest.controller;
 
+import com.dev.domain.entity.Cliente;
+import com.dev.domain.repository.ClientesRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/api/clientes") //indica que esse caminho vai levar a essa classe
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET) //Indica que esse caminho leva a esse método
-    @ResponseBody //Indica que a string que vai retornar é o corpo da resposta
-    public String helloCLiente(@PathVariable("nome") String nome) {
-        return String.format("Hello, %s!", nome);
+    private ClientesRepository clientesRepository;
+
+    public ClienteController(ClientesRepository clientesRepository){
+        this.clientesRepository = clientesRepository;
+    }
+
+    @GetMapping("/{id}") //É o RequestMapping com o método GET definido
+    @ResponseBody
+    public ResponseEntity getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientesRepository.findById(id);
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
